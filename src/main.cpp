2515d55492b9b3d1a59a29801513bca6ba159900
MirorE <ericrfranco219@gmail.com>
2021-10-18 19:58:54 -0400

    Apply force to a body in space

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,28 @@
+#include <array>
+#include <algorithm>
+
+static int constexpr DIMENSIONS = 2;
+static float constexpr DT = 0.01;
+
+struct Body {
+    float mass;
+    std::array<float, DIMENSIONS> position;
+    std::array<float, DIMENSIONS> velocity;
+    std::array<float, DIMENSIONS> acceleration;
+    
+};
+
+void update(Body &b, std::array<float, DIMENSIONS> const& force) {
+    std::transform(std::begin(force), std::end(force), std::begin(b.acceleration), [&b](auto coord){return coord / b.mass;});
+    for(int i = 0; i < b.velocity.size(); ++i) {
+        b.velocity[i] = DT * b.acceleration[i];
+    }
+
+    for(int i = 0; i < b.position.size(); ++i) {
+        b.position[i] = DT * b.velocity[i];
+    }
+}
+
 int main() {
     return 0;
 }
